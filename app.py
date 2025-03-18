0a79a88d667268f761065369afa91f6395366303
Oscar-Qiu <yiheng.qiu@ufl.edu>
2025-03-17 21:49:43 -0400

    Add database with flask

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
-from flask import url_for
-from flask import render_template
+from flask import Flask, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy  # 注意大小写
+import os
+import click
 app = Flask(__name__)
+
+# 配置数据库（必须在 db = SQLAlchemy(app) 之前）
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+
+# 初始化 db 对象
+db = SQLAlchemy(app)
+
+# 示例数据
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
@@ -10,9 +20,56 @@ movies = [
 name = "oscar"
 
 @app.route("/")
-def hello():
-    return "<h1>Hello Totoro<h1> <img src='https://helloflask.com/totoro.gif'>"
-@app.route("/welcome")
 def index():
-    return render_template("index.html",name=name,movies=movies)
-    
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template("index.html", user=user, movies=movies)
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')  
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+# 定义模型
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+# 确保 db 对象可以被导入
+__all__ = ['app', 'db']
